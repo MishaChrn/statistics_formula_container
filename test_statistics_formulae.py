dcc9a63f9b7a68efb65ac5e8e7702dc14737aef4
M_Chrn_W <91328364+MishaChrn@users.noreply.github.com>
2026-02-17 02:54:44 +0900

    Working on def test_calculate_quartiles_with_statistics_module(): statistics does not result correct quartiles, numpy didn't work

--- test_statistics_formulae.py
+++ test_statistics_formulae.py
@@ -2,6 +2,7 @@
 import pytest
 import statistics
 from statistics_formulae import calculate_median_without_statistics_module
+import numpy as np
 
 def test_calculate_arithmetic_mean_without_statistics_module():
     # Calculates the arithmetic mean (平均値) from the elements in mean_target_values.
@@ -57,6 +58,7 @@ def test_calculate_mode_with_statistics_module():
 @pytest.mark.parametrize(
     "quartiles_target_values, quartiles_1_expected, quartiles_2_expected, quartiles_3_expected",
     [([3.0, 4.0, 5.5, 3.5, 4.2, 3.4, 2.8, 4.2, 4.7, 2.2, 3.8], 3.0, 3.8, 4.2),
+     ([3.0, 4.0, 5.5, 3.5, 4.2, 3.4, 2.8, 4.2, 4.7, 2.2, 3.8, 4.6], 3.2, 3.9, 4.4),
      ([3.0, 4.0, 5.5, 3.5, 4.2, 3.4, 2.8, 4.2, 4.7, 2.2, 3.8, 4.6], 3.2, 3.9, 4.4)]
 )
 def test_calculate_quartiles_without_statistics_module(quartiles_target_values, quartiles_1_expected, quartiles_2_expected, quartiles_3_expected):
@@ -90,29 +92,22 @@ def test_calculate_quartiles_without_statistics_module(quartiles_target_values, 
     assert quartiles_3 == quartiles_3_expected
 
 @pytest.mark.parametrize(
-    "quartiles_target_values, quartiles_1_expected, quartiles_2_expected, quartiles_3_expected",
+    "quartiles_target_values_with, quartiles_1_expected_with, quartiles_2_expected_with, quartiles_3_expected_with",
     [([3.0, 4.0, 5.5, 3.5, 4.2, 3.4, 2.8, 4.2, 4.7, 2.2], 3.0, 3.75, 4.2),
      ([3.0, 4.0, 5.5, 3.5, 4.2, 3.4, 2.8, 4.2, 4.7, 2.2, 3.8], 3.0, 3.8, 4.2),
      ([3.0, 4.0, 5.5, 3.5, 4.2, 3.4, 2.8, 4.2, 4.7, 2.2, 3.8, 4.6], 3.2, 3.9, 4.4)]
 )
-def test_calculate_quartiles_with_statistics_module(quartiles_target_values, quartiles_1_expected, quartiles_2_expected, quartiles_3_expected):
-    quartiles_1, quartiles_2, quartiles_3 = statistics.quantiles(quartiles_target_values, n=4)
-    assert quartiles_1 == quartiles_1_expected
-    assert quartiles_2 == quartiles_2_expected
-    assert quartiles_3 == quartiles_3_expected
+def test_calculate_quartiles_with_statistics_module(quartiles_target_values_with, quartiles_1_expected_with, quartiles_2_expected_with, quartiles_3_expected_with):
+    quartiles_1, quartiles_2, quartiles_3 = statistics.quantiles(quartiles_target_values_with, n=4) # Errors with wrong results
+    # quartiles_1 = np.percentile(quartiles_target_values_with, 25)
+    # quartiles_2 = np.percentile(quartiles_target_values_with, 50)
+    # quartiles_3 = np.percentile(quartiles_target_values_with, 75)
+    assert quartiles_1 == pytest.approx(quartiles_1_expected_with)
+    assert quartiles_2 == pytest.approx(quartiles_2_expected_with)
+    assert quartiles_3 == pytest.approx(quartiles_3_expected_with)
 
 # FAILED test_statistics_formulae.py::test_calculate_quartiles_with_statistics_module[quartiles_target_values1-3.2-3.9-4.4] - assert 3.1 == 3.2
 # 偶数個のデータの場合にエラーになってしまった。
+# numpy使っても計算が合わないよ～
+# .approx()を使っても許容範囲に該当しない。
 
-"""
-代替案：
-import numpy as np
-
-
-data = [12, 34, 57, 22, 97, 51, 69, 87, 72, 62]
-
-#四分位数をそれぞれ計算
-q1 = np.percentile(data, 25)  # 第一四分位数
-q2 = np.percentile(data, 50)  # 第二四分位数（中央値）
-q3 = np.percentile(data, 75)  # 第三四分位数
-"""
